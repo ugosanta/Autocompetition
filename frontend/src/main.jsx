0034d428d70328929633f0e70dc08eb790e4a50c
ugosanta <santarelli.ugo@gmail.com>
2026-02-17 09:34:45 +0100

    C'est compliqué

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './app';
 
 //lien avec root dans l'index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //render de l'app dans le root
-ReactDOM.createRoot(rootElement).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of our application.</p>
+    </div>
+  );
+}
+
+export default Home;
