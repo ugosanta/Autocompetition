5a39c4c7be7b66914d70e2347376f6f54f76e969
ugosanta <santarelli.ugo@gmail.com>
2026-02-17 14:18:55 +0100

    je galère à faire afficher un truc

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,23 @@
 function Home() {
   return (
-    <div className="home-container">  
-      <h1 className="text-3xl font-bold mb-4">Bienvenue sur AutoCompetition</h1>
-      <p className="text-lg mb-6">Votre plateforme de compétition automobile en ligne</p>
-    </div> 
+    <main>
+      {/* Les balises de titre (h1 à h6) */}
+      <h1 className="titre-principal">Bienvenue sur mon site</h1>
+      
+      {/* Les paragraphes */}
+      <p className="description">
+        Ceci est mon premier paragraphe de texte affiché sur ma page d'accueil.
+      </p>
+
+      {/* Les listes */}
+      <ul>
+        <li>Élément de liste 1</li>
+        <li>Élément de liste 2</li>
+      </ul>
+
+      {/* Texte simple sans balise spécifique (possible mais moins recommandé) */}
+      <span>Petit texte informatif.</span>
+    </main>
   );
 }
 
