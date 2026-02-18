46800c30683302f30f66c82e335d0a1540304319
ugosanta <santarelli.ugo@gmail.com>
2026-02-18 09:33:01 +0100

    Que personne ne voit ça...

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Route, Switch, Routes } from "react-router-dom
 import Home from "./pages/home";
 
 function App() {
+
     return (
         <Router>
             <Nav/>
             <Routes>
                 <Route path="/" element={<Home />} />
-                
+
             </Routes>
             <Footer/>
         </Router>
 
     );
 };  
+
 export default App;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,24 +1,16 @@
-function Home() {
+  import { useEffect } from "react";
+  
+  function Home() {
+      useEffect(() => {
+        document.title = "Home - AutoCompetition";
+}, []);
+
   return (
-    <main>
-      {/* Les balises de titre (h1 à h6) */}
-      <h1 className="titre-principal">Bienvenue sur mon site</h1>
-      
-      {/* Les paragraphes */}
-      <p className="description">
-        Ceci est mon premier paragraphe de texte affiché sur ma page d'accueil.
-      </p>
-
-      {/* Les listes */}
-      <ul>
-        <li>Élément de liste 1</li>
-        <li>Élément de liste 2</li>
-      </ul>
-
-      {/* Texte simple sans balise spécifique (possible mais moins recommandé) */}
-      <span>Petit texte informatif.</span>
-    </main>
-  );
-}
-
+    <div>
+      <h1>
+        Bienvenue sur AutoCompetition, votre plateforme de vente de véhicules de compétition automobile en ligne !
+      </h1>
+    </div>
+    );}
+    
 export default Home;
