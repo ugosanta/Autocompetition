95e8ec81827ae2649730ff7676e7b1689ceb551a
ugosanta <santarelli.ugo@gmail.com>
2026-02-19 14:30:57 +0100

    Création d'une barre de commande en haut du site, le logo redirige bien vers la page d'accueil, il reste à voir si tout est responsive et à créer des boutons.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,12 +6,15 @@
           document.title = "À propos - Autocompetition";
   }, []);
   
-    return (
-      <div>
-        <h1>
-          Vous êtes sur la page pour nous contacter !
-        </h1>
-      </div>
-      );}
+  return (
+<div class="mx-25 flex max-w-full items-center gap-x-4 bg-white p-4 shadow-lg outline outline-black/5 rounded-b-xl dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  
+  <a href="/">
+    <img class="size-20 shrink-5" src="./src/assets/favicon.ico" alt="Insert Logo" 
+      
+      />
+    </a>
+  </div>
+    );}
       
   export default About;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,9 +7,13 @@
 }, []);
 
   return (
-<div class="mx-auto flex max-w-full items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  <img class="size-20 shrink-5" src="./src/assets/favicon.ico" alt="Insert Logo" />
-
+<div class="mx-25 flex max-w-full items-center gap-x-4 bg-white p-4 shadow-lg outline outline-black/5 rounded-b-xl dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  
+  <a href="/">
+    <img class="size-20 shrink-5" src="./src/assets/favicon.ico" alt="Insert Logo" 
+      
+      />
+    </a>
   </div>
     );}
     
