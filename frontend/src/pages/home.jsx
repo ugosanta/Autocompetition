3e431d12b340b029a34ab0e0e6917304b967165e
ugosanta <santarelli.ugo@gmail.com>
2026-02-19 09:29:41 +0100

    Première forme avec shrek sur la page home !

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,11 +7,10 @@
 }, []);
 
   return (
-    <div>
-      <h1>
-        Bienvenue sur Autocompétition, votre plateforme de vente de véhicules de compétition automobile !
-      </h1>
-    </div>
+<div class="mx-auto flex max-w-full items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  <img class="size-20 shrink-5" src="./src/assets/favicon.ico" alt="Insert Logo" />
+
+  </div>
     );}
     
 export default Home;
