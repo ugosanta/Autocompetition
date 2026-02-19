3eb7c02be1ddc1ae90794b1d5b6a6f891375483d
ugosanta <santarelli.ugo@gmail.com>
2026-02-19 15:39:24 +0100

    mmh on repars à 0 niveau bouton de thème

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Retrait de Switch
-import ThemeToggle from "./components/Themeswitch";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profil from "./pages/Profil";
@@ -8,17 +7,14 @@ import Profil from "./pages/Profil";
 
 function App() {
     return (
+
         <Router>
-            <div className="min-h-screen bg-white dark:bg-slate-900 text-black dark:text-white transition-colors duration-300"></div>
-                <ThemeToggle />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/profil" element={<Profil />} />
                 </Routes>
-        <div />
-    </Router>
+        </Router>
     );
-};  
-
+};
 export default App;
